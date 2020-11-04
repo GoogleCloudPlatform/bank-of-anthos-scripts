18bd51db8b63e12db58b3966b9b71941fcab0d29
osmondngwk <28585691+osmondngwk@users.noreply.github.com>
2020-11-03 18:01:37 -0800

    Update connect-hub.sh

--- install/gke/connect-hub.sh
+++ install/gke/connect-hub.sh
@@ -26,7 +26,7 @@ export CLUSTER_ZONE=$2
 export CLUSTER_KUBECONFIG=$WORK_DIR/${CLUSTER_NAME}/central.context
 
 #export USER="`whoami`@google.com"
-export USER="`gcloud auth list --filter=status:ACTIVE --format=\"value(account)\"`"
+export USER=$(gcloud auth list --filter=status:ACTIVE --format="value(account)")
 export SVC_ACCT_NAME="${CLUSTER_NAME}-connect"
 export LOCAL_KEY_PATH="./workdir/${SVC_ACCT_NAME}"
 
