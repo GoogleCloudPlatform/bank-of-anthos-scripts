3a123ffb33523bcb5050013c47be0b9dd5e1419e
amr1k <54102529+amr1k@users.noreply.github.com>
2021-02-09 13:57:00 +0000

    Added ACM

--- install/spin-up-lab5-part2.sh
+++ install/spin-up-lab5-part2.sh
@@ -16,5 +16,57 @@ export PROJECT_ID=$(gcloud config get-value project)
 export EMAIL=$(gcloud config get-value account)
 export ROOT=$HOME/hybrid-sme
 
+export REPO_URL=https://source.developers.google.com/p/${PROJECT_ID}/r/config-repo
+git config credential.helper gcloud.sh
+gcloud source repos create config-repo
+cd $ROOT
+gcloud source repos clone config-repo
+cd config-repo
+
+cp -r $ROOT/bank-of-anthos-scripts/install/acm/config-repo-source/* .
+
+git add .
+git commit -m "Initialize config-repo"
+git push -u origin master
+
+
+echo "🔑 Granting ACM config-repo access..."
+# do *not* run ssh-keygen again. this key persists in the user's environment after cleanup.
+
+kubectx gcp
+kubectl create secret generic git-creds \
+--namespace=config-management-system \
+--from-file=ssh=$HOME/.ssh/id_rsa.sme
+
+kubectx onprem
+kubectl create secret generic git-creds \
+--namespace=config-management-system \
+--from-file=ssh=$HOME/.ssh/id_rsa.sme
+
+echo "⬇️ Creating ConfigManagement CRD in both clusters..."
+export ONPREM=onprem
+export GCP=gcp
+export BASE_DIR=$ROOT/bank-of-anthos-scripts/install
+SSH_REPO_URL=ssh://${GCLOUD_ACCOUNT}@source.developers.google.com:2022/p/${PROJECT_ID}/r/config-repo
+
+kubectx $ONPREM
+cat $BASE_DIR/acm/config_sync.yaml | \
+  sed 's|<REPO_URL>|'"$SSH_REPO_URL"'|g' | \
+  sed 's|<CLUSTER_NAME>|'"$ONPREM"'|g' | \
+  sed 's|none|ssh|g' | \
+  kubectl apply -n config-management-system -f -
+
+kubectx $GCP
+cat $BASE_DIR/acm/config_sync.yaml | \
+  sed 's|<REPO_URL>|'"$SSH_REPO_URL"'|g' | \
+  sed 's|<CLUSTER_NAME>|'"$GCP"'|g' | \
+  sed 's|none|ssh|g' | \
+  kubectl apply -n config-management-system -f -
+
+
+echo "✅ Lab 1 fast track complete!"
+
+
+
 cd $ROOT/bank-of-anthos-scripts/deploy-app/
 ./fasttrack.sh
